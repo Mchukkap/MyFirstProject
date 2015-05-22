9485b844f7746958a832b0e806d63235964e533b
Manasa <mchukkap@gmail.com>
2015-05-21 20:54:49 -0400

    Update

--- SriRama/src/HelloDeepu.java
+++ SriRama/src/HelloDeepu.java
@@ -1,8 +1,21 @@
 
 public class HelloDeepu {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Kanna....!");
-	}
 
-}
+	public static void main(String[] args) {
+		
+		String name;
+		name="Munni";
+		System.out.println(name);
+		System.out.println("\"munni\"");
+		int count=8;
+		count *= 2;
+		System.out.println(count);
+		System.out.println(count++);
+		System.out.println(count);
+		
+	}
+	}
+            
+
+
---
+++ SriRama/src/loopmyexample.java
@@ -0,0 +1,37 @@
+
+public class loopmyexample {
+
+	public static void main(String[] args) {
+	
+		int myage = 22;
+		String name="munnis";
+		
+       if(myage>21)
+       {
+    	   System.out.println("Adult");
+    	  if(name=="munni")
+    	  {
+    		  System.out.println("This is Manasa");
+    	  }
+    	 else{
+    		  System.out.println("Not Munni");
+    	  }
+       }
+       else{
+    	   System.out.println("This is a child");
+       }
+      /* else if(myage==21)
+       {
+    	   
+    	   System.out.println("just turned 21");
+       }
+       else if(myage<21)
+       {
+    	   System.out.println("get out of here");
+       }
+       else
+       {
+    	   System.out.println("do nothing");
+       }*/
+}
+}
